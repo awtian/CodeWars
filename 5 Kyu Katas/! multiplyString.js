5526bafebddb5cf61191e90a64a997423c3906a9
awtian <say@awtian.com>
2017-12-28 15:02:37 +0700

    adding multiplyString

---
+++ 5 Kyu Katas/! multiplyString.js
@@ -0,0 +1,33 @@
+function multiply(a, b) {
+    if (+a == 0 || +b == 0) {
+        return '0';
+    }
+
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    var result = [];
+
+    for (var i = 0; a[i] >= 0; i++) {
+        for (var j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (var i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+    result = result.reverse().join('');
+    while (result[0] == 0) {result = result.slice(1)};
+    return result;
+}
